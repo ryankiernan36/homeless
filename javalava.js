2097cab73661a84264710fda742bc924dcb65cf0
Ryan Kiernan <x16329476@student.ncirl.ie>
2016-12-18 12:15:34 +0000

    homeless

---
+++ javalava.js
@@ -0,0 +1,24 @@
+<script> 
+		var homeless=confirm("are you homeless?");
+		if(homeless==false){
+			alert("please help");
+		}
+		else{
+			var loca=prompt("what province are u based in?");
+		}
+			if(loca=="leinster" || loca=="Leinster"){
+			alert("CALL 1800 707 707 between the hours of 2pm to 1am soup kitchen and people to talk to on OConnel street every night and check out the facebook page linked");
+			}
+			else if(loca=="Ulster" || loca=="ulster"){
+			alert("CALL 1800 707 707 and they will help sort a hostel out for you tonight")
+			}
+			else if(loca=="connaught" || loca=="Connaught"){
+			alert("CALL 1800 707 707 and they will help sort a hostel out for you tonight")
+			}
+			else if(loca=="munster" || loca=="Munster"){
+			alert("CALL 1800 707 707 and they will help sort a hostel out for you tonight")
+			}
+			else{
+			alert("Province not recognised")
+			}
+</script>
